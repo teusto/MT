0eaf3b1bfc8698cad93f59573af151801041e589
Matheus Toscano <pteutoscano@gmail.com>
2024-10-17 12:38:49 +0100

    Refactoring the transitions animations functions from homepage to all pages

--- src/scripts/transition.js
+++ src/scripts/transition.js
@@ -1,79 +1,166 @@
-const leaveAnimation = (container) => {
-    const boxesUp = [...container.querySelectorAll('#contact'), ...container.querySelectorAll('#portfolio')];
-    const boxesDown = [...container.querySelectorAll('#experience')];
-    const title = container.querySelector('#education p');
-    const tl = gsap.timeline({
-        defaults:{
-            ease: 'expo.inOut',
-            duration: 2
-        }
-    })
+const leaveAnimation = (container, bxup, bxdown, titleSection) => {
+  let boxesUp = [];
+  let boxesDown = [];
 
-    tl.to([boxesUp, boxesDown], {
-        height: 0,
-        autoAlpha: .2,
-        fontSize: 0
-    }, 1).to(title, {
+  bxup.forEach((element) => {
+    return boxesUp.push(container.querySelectorAll(element));
+  });
+  bxdown.forEach((element) => {
+    return boxesDown.push(container.querySelectorAll(element));
+  });
+
+  const title = container.querySelector(`${titleSection} p`);
+  const tl = gsap.timeline({
+    defaults: {
+      ease: "expo.inOut",
+      duration: .8,
+    },
+  });
+
+  tl.to(
+    [boxesUp, boxesDown],
+    {
+      height: 0,
+      autoAlpha: 0,
+      fontSize: 0,
+    },
+    1
+  )
+    .to(
+      title,
+      {
         color: "#fff8e8",
         opacity: 0,
-        duration: .8
-    }, 1).to(container, {
+        duration: 0.5,
+      },
+      1
+    )
+    .to(
+      container,
+      {
         backgroundColor: "#1E1E1E",
-        duration: .8
-    }, 1).eventCallback("onComplete", function remove(){
-        container.remove()
-    })
-    
-    return tl;
-}
+        duration: 0.5,
+      },
+      1
+    )
+    .eventCallback("onComplete", function remove() {
+      container.remove();
+    });
 
-const enterAnimation = (container) => {
-    const boxExpand = [...container.querySelectorAll('#education')];
-    const infoContainer = container.querySelector('.info');
-    const title = container.querySelector('#education p');
-    let svg = container.querySelectorAll('svg path');
-    console.log(svg)
-    const tl = gsap.timeline({
-        defaults:{
-            ease: 'expo.inOut',
-            duration: .7
-        }
-    })
+  return tl;
+};
 
-    tl
-    .to(boxExpand, {
-        height: 380,
-    },1)
-    .from(title,{
+const enterAnimation = (container, pageBox, expandtToHeight) => {
+  const boxExpand = [...container.querySelectorAll(pageBox)];
+  const infoContainer = container.querySelector(".info");
+  const title = container.querySelector(`${pageBox} p`);
+  let svg = container.querySelectorAll("svg path");
+  console.log(svg);
+  const tl = gsap.timeline({
+    defaults: {
+      ease: "expo.inOut",
+      duration: 0.7,
+    },
+  });
+
+  tl.to(
+    boxExpand,
+    {
+      height: expandtToHeight,
+    },
+    1
+  )
+    .from(
+      title,
+      {
         opacity: 0,
-    },1)
-    .to(svg, {
+      },
+      1
+    )
+    .to(
+      svg,
+      {
         fill: "#fff8e8",
-    }, 1)
+      },
+      1
+    )
     .from(infoContainer, {
-        autoAlpha: 0,
-        scale: 0,
-    })
+      autoAlpha: 0,
+      scale: 0,
+    });
 
-    return tl;
-}
+  return tl;
+};
 
 barba.init({
-    transitions: [{
-      name: 'education-transition',
+  transitions: [
+    {
+      name: "education-transition",
       from: {
-        namespace:["homepage"]
+        namespace: ["homepage"],
       },
       to: {
-        namespace: ["education"]
+        namespace: ["education"],
       },
-      leave({current}) {
+      leave({ current }) {
         // create your stunning leave animation here
-        return leaveAnimation(current.container);
+        return leaveAnimation(current.container,["#contact", "#portfolio"],["#experience"],"#education");
       },
-      enter({next}) {
+      enter({ next }) {
         // create your amazing enter animation here
-        return enterAnimation(next.container);
-      }
-    }]
+        return enterAnimation(next.container, "#education", 230);
+      },
+    },
+    {
+      name: "contact-transition",
+      from: {
+        namespace: ["homepage"],
+      },
+      to: {
+        namespace: ["contact"],
+      },
+      leave({ current }) {
+        // create your stunning leave animation here
+        return leaveAnimation(current.container,["#portfolio"],["#experience", "#education"],"#contact");
+      },
+      enter({ next }) {
+        // create your amazing enter animation here
+        return enterAnimation(next.container, "#contact", 400);
+      },
+    },
+    {
+      name: "experience-transition",
+      from: {
+        namespace: ["homepage"],
+      },
+      to: {
+        namespace: ["experience"],
+      },
+      leave({ current }) {
+        // create your stunning leave animation here
+        return leaveAnimation(current.container,["#contact", "#portfolio"],["#education"],"#experience");
+      },
+      enter({ next }) {
+        // create your amazing enter animation here
+        return enterAnimation(next.container, "#experience", 420);
+      },
+    },
+    {
+      name: "experience-transition",
+      from: {
+        namespace: ["homepage"],
+      },
+      to: {
+        namespace: ["portfolio"],
+      },
+      leave({ current }) {
+        // create your stunning leave animation here
+        return leaveAnimation(current.container,["#contact"],["#education", "#experience"],"#portfolio");
+      },
+      enter({ next }) {
+        // create your amazing enter animation here
+        return enterAnimation(next.container, "#portfolio", 450);
+      },
+    },
+  ],
 });
